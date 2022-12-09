a1ebdeeee2bdf2ac9be669a083b5a171ebfffbe3
Gzim <gzim.ibraimi@gmail.com>
2022-12-09 16:21:31 +0100

    Updated and cleaned

--- src/Quote.js
+++ src/Quote.js
@@ -1,8 +1,18 @@
-fetch("https://zenquotes.io/api/quotes/").then((res) => { return res.json();
-})
-      .then((objectData)=>{
+fetch("https://zenquotes.io/api/quotes").then((data)=>{
+    //console.log(data)
+    return data.json();
+}).then((quoteData)=>{
+    console.log(quoteData[0].q);
     let tableData="";
-    objectData.map((values)=>{
-        tableData='<h1>${values.q}</h1>'
-    })
+    let id=1;
+    quoteData.map((values)=>{
+        tableData+=`<tr>
+        <td>${id++} </td>
+        <td>${values.q}</td>
+        <td>${values.a}</td>
+      </tr>`;
+    });
+    document.getElementById("table_body").innerHTML=tableData;
+}).catch((e)=>{
+    console.log(e);
 })
